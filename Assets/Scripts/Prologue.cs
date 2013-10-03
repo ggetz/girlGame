b0b1d90854cd7fce92b1f710216d791627ba8493
microlm <laura.mo@gmail.com>
2013-10-03 14:44:44 -0400

    Finished Prologue

--- Assets/Scripts/Prologue.cs
+++ Assets/Scripts/Prologue.cs
@@ -13,6 +13,10 @@ public class Prologue: MonoBehaviour
 	GSpineSprite eye;
 	GSpineSprite mouth;
 	FLabel text;
+	int fade;
+	float fadeRate;
+	bool textFading;
+	bool fadeType;
 	int time=0;
 	
 	void Start()
@@ -55,6 +59,7 @@ public class Prologue: MonoBehaviour
 		textFont = "Incubus";
 		
 		text = new FLabel(textFont, "");
+		text.scale=0.6f;
 	}
 			
 	void SetUpStage()
@@ -66,10 +71,11 @@ public class Prologue: MonoBehaviour
 		Futile.stage.AddChild (hand);
 		Futile.stage.AddChild(text);
 		
-		wholeShot.SetPosition(1920*0.6f/2f, 0);
-		eye.SetPosition(1920*0.6f/2f, 0);
-		hand.SetPosition(1920*0.6f/2f, 0);
-		mouth.SetPosition(1920*0.6f/2f, 0);
+		wholeShot.SetPosition(Futile.screen.width/2f, 0);
+		eye.SetPosition(Futile.screen.width/2f, 0);
+		hand.SetPosition(Futile.screen.width/2f, 0);
+		mouth.SetPosition(Futile.screen.width/2f, 0);
+		text.SetPosition (Futile.screen.width/2f, Futile.screen.height/2f);
 		
 		wholeShot.alpha = 0f;
 		eye.alpha=0f;
@@ -89,6 +95,16 @@ public class Prologue: MonoBehaviour
 			wholeShot.Play("Part1", false);
 		}
 		
+		if(time==250)
+		{
+			text.text="They can't hear us but we cry\nas if they could";
+			fadeText (true, 50);
+		}
+		
+		if(time==450)
+		{
+			fadeText (false, 50);
+		}
 		if(time==700)
 		{
 			wholeShot.alpha=0;
@@ -96,29 +112,53 @@ public class Prologue: MonoBehaviour
 			mouth.Play("Part1", false);
 		}
 		
-		if(time==1100)
+		if(time==950)
+		{
+			text.text="Even if only for an instant\nlonger,even if we will fade\none day...";
+			fadeText (true, 50);
+		}
+		if(time==1150)
+		{
+			fadeText (false, 50);
+		}
+		
+		if(time==1200)
 		{
 			mouth.alpha=0;
+			wholeShot.Stop ();
 			wholeShot.alpha=100;
 			wholeShot.Play("Part2", false);
 		}
 		
-		if(time==1250)
+		if(time==1350)
 		{
 			wholeShot.alpha=0;
 			eye.alpha=100;
 			eye.Play("animation", false);
 		}
 		
+		if(time==1500)
+		{
+			text.text="Please.\n\nNot Yet";
+			fadeText (true, 50);
+		}
+		
 		if(time==1700)
 		{
+			fadeText (false,50);
+		}
+		
+		if(time==1800)
+		{
 			eye.alpha=0;
+			eye.Stop ();
+			wholeShot.Stop ();
 			wholeShot.alpha=100;
-			eye.Stop();
+			
 			wholeShot.Play("Part3", false);
 		}
 		
-		if(time==1900)
+		if(time==2000)
 		{
 			wholeShot.Stop();
 			wholeShot.alpha=0;
@@ -126,26 +166,80 @@ public class Prologue: MonoBehaviour
 			hand.Play("Part1", false);
 		}
 		
-		if(time==2300)
+		if(time==2400)
 		{
 			hand.alpha=0;
 			wholeShot.alpha=100;
 			wholeShot.Play("Part4", false);
 		}
 		
-		if(time==2950)
+		if(time==3000)
+		{
+			text.text="So you over there, the\none who can hear us.";
+			fadeText (true, 50);
+		}
+		
+		if(time==3150)
+		{
+			fadeText (false, 50);
+		}
+		
+		if(time==3000)
 		{
 			wholeShot.alpha=0;
 			hand.alpha=100;
 			hand.Play("Part2", false);
 		}
 		
+		if(time==3500)
+		{
+			text.text="Won't you carry us far\naway from here?";
+			fadeText (true, 50);
+		}
+		if(time==3800)
+		{
+			fadeText (false, 50);
+		}
+		
 		time++;
+		
+		if(textFading)
+		{
+			if(fade<0)
+			{
+				fadeText (fadeType, fade);
+			}
+			if(fadeType)
+			{
+				text.alpha+=fadeRate;
+			}
+			else
+			{
+				text.alpha-=fadeRate;
+			}
+			fade--;
+		}
 	}
 	
-	void textFade(bool fadeIn, int startTime)
+	void fadeText(bool fadeIn, int fadeTime)
 	{
-			
+		Debug.Log (fadeTime);
+		Debug.Log (fadeIn);
+		if(fadeTime > 0 && !textFading)
+		{
+			textFading=true;
+			fadeRate=1f/fadeTime;
+			fade=fadeTime;
+			fadeType=fadeIn;
+		}
+		
+		if(fadeTime <=0 && textFading)
+		{
+			textFading=false;
+			fadeRate=0;
+			fadeTime=0;
+			fadeType=false;
+		}
 	}
 	
 }
