8d86f448821e41f9b4e321a27ef7f4297a63e882
ggetz <ggetz881@gmail.com>
2013-10-08 13:08:10 -0400

    playing with intersecting

--- Assets/Scripts/Rectangle.cs
+++ Assets/Scripts/Rectangle.cs
@@ -65,8 +65,9 @@ public class Rectangle {
 	
 	public bool isIntersecting(Rectangle r)
 	{
-		if ( ( ( r.right() >= left () && r.right() <= right() ) || r.left() >= left () && r.right() <= right())//horizontally
-			&& (( r.top () >= bottom() && r.top() <= top() )  || ( r.bottom () >= bottom() && r.bottom() <= top() ) ) )//vertically
+		
+		if ( ( ( r.right() >= left() && r.right() <= right() )))// || (r.left() >= left () && r.left() <= right())))//horizontally
+			//&& (( r.top () >= bottom() && r.top() <= top() )  || ( r.bottom () >= bottom() && r.bottom() <= top() ) ) )//vertically
 			return true;
 		else return false;
 	}
